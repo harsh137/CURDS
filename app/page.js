d1c18cce7a3f541c1facbe2c99c9295b4d9684f6
Harsh gupta <guptaharsh137@gmail.com>
2024-05-16 11:26:44 +0530

    New Update

--- app/page.js
+++ app/page.js
@@ -24,7 +24,6 @@ export default function Home() {
 
   useEffect(() => {
     fetchEntries();
-    
   }, []);
 
   const toggleDarkMode = () => {
@@ -36,14 +35,16 @@ export default function Home() {
       document.documentElement.classList.remove("dark");
     }
   };
+
   //FEATCHING ENTRIES API CALL
   const fetchEntries = async () => {
     try {
-      const res = await fetch("api/getEntries", { method: "GET", cache: 'no-store'  });
+      const res = await fetch("/api/getEntries", { method: "GET", cache: 'no-store' });
+      if (!res.ok) throw new Error('Network response was not ok');
       const data = await res.json();
       setEntries(data);
     } catch (error) {
-      console.log(error);
+      console.log('Fetch entries failed: ', error);
     }
   };
 
@@ -52,7 +53,7 @@ export default function Home() {
     try {
       const response = await fetch("/api/addEntry", {
         method: "POST",
-        cache: 'no-store' ,
+        cache: 'no-store',
         body: JSON.stringify(formData),
         headers: {
           "Content-Type": "application/json",
@@ -61,19 +62,22 @@ export default function Home() {
 
       if (response.ok) {
         fetchEntries();
+      } else {
+        console.log('Failed to save entry');
       }
     } catch (error) {
-      console.log(error);
+      console.log('Save entry failed: ', error);
     }
   };
 
   //DELETE ENTRY API CALL
   const handleDelete = async (id) => {
     try {
-      await fetch(`/api/deleteEntries/${id.toString()}`, { method: "DELETE",cache: 'no-store'  });
+      const response = await fetch(`/api/deleteEntries/${id}`, { method: "DELETE", cache: 'no-store' });
+      if (!response.ok) throw new Error('Delete entry failed');
       fetchEntries();
     } catch (error) {
-      console.log(error);
+      console.log('Delete entry failed: ', error);
     }
   };
 
@@ -94,7 +98,7 @@ export default function Home() {
     try {
       const response = await fetch(`/api/updateEntry/${formData.id}`, {
         method: "PUT",
-        cache: 'no-store' ,
+        cache: 'no-store',
         body: JSON.stringify(formData),
         headers: {
           "Content-Type": "application/json",
@@ -103,11 +107,14 @@ export default function Home() {
 
       if (response.ok) {
         fetchEntries();
+      } else {
+        console.log('Failed to update entry');
       }
     } catch (error) {
-      console.log(error);
+      console.log('Update entry failed: ', error);
     }
   };
+
   //HANDLE SELECTED ENTRY
   const handleSelect = (entry, isSelected) => {
     if (isSelected) {
@@ -116,18 +123,18 @@ export default function Home() {
       setSelectedEntries(selectedEntries.filter((e) => e._id !== entry._id));
     }
   };
+
   // SEND EMAIL API CALL
   const handleSendEmail = async () => {
     setSending(true);
     console.log(selectedEntries);
-  
+
     try {
       const response = await fetch("/api/sendEmail", {
         method: "POST",
-        cache: 'no-store' ,
+        cache: 'no-store',
         headers: {
           "Content-Type": "application/json",
-          
         },
         body: JSON.stringify({ selectedRows: selectedEntries }),
       });
@@ -141,7 +148,7 @@ export default function Home() {
       }
     } catch (e) {
       setSending(false);
-      console.log(e);
+      console.log('Send email failed: ', e);
       alert("Error sending email: An unexpected error occurred");
     }
   };
@@ -156,57 +163,56 @@ export default function Home() {
   );
 
   return (
-    <div className={`w-[100%] mx-auto p-4 min-h-screen bg-slate-300 dark:bg-[#121212] dark:text-slate-50`}>
-  <div className="flex flex-col md:flex-row justify-between items-center mb-8">
-    <h1 className="text-3xl font-bold text-center mb-4 md:mb-0 md:flex-grow">Manage Entries</h1>
-    <button
-      onClick={toggleDarkMode}
-      className="dark:bg-white dark:text-black bg-gray-500 px-4 py-2 rounded focus:outline-none"
-    >
-      {darkMode ? "Light Mode" : "Dark Mode"}
-    </button>
-  </div>
-  <div className="flex flex-col md:flex-row justify-center items-center mb-8 w-full">
-    <input
-      type="text"
-      placeholder="Search by any attribute"
-      value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)}
-      className="px-4 py-2 text-black rounded-l-full border border-gray-400 focus:outline-none focus:ring focus:border-blue-500 flex-grow w-full md:w-96 bg-slate-100 dark:bg-gray-700 dark:text-white dark:border-gray-600 mb-4 md:mb-0"
-    />
-    <Button
-      onClick={() => setIsOpen(true)}
-      loading={sending}
-      className="rounded-r-full md:ml-2 w-full md:w-auto"
-    >
-      Add New Data
-    </Button>
-  </div>
-  <div className="overflow-x-auto w-full flex-grow">
-    <Table
-      entries={filteredEntries}
-      onEdit={handleEdit}
-      onDelete={handleDelete}
-      onSelect={handleSelect}
-    />
-  </div>
-  <div className="flex  justify-center mt-8 w-full">
-    <Button onClick={handleSendEmail} loading={sending} className="w-full md:w-auto">
-      {sending ? "Sending Email..." : "Send Email"}
-    </Button>
-  </div>
-  <FormModal
-    isOpen={isOpen}
-    onClose={() => setIsOpen(false)}
-    onSave={handleSave}
-  />
-  <FormUpdate
-    isOpenUpdate={isOpenUpdate}
-    onCloseUpdate={() => setIsOpenUpdate(false)}
-    onSaveUpdate={handleSaveUpdate}
-    data={updateSelect}
-  />
-</div>
-
+    <div className={`w-full mx-auto p-4 min-h-screen bg-slate-300 dark:bg-[#121212] dark:text-slate-50`}>
+      <div className="flex flex-col md:flex-row justify-between items-center mb-8">
+        <h1 className="text-3xl font-bold text-center mb-4 md:mb-0 md:flex-grow">Manage Entries</h1>
+        <button
+          onClick={toggleDarkMode}
+          className="dark:bg-white dark:text-black bg-gray-500 px-4 py-2 rounded focus:outline-none"
+        >
+          {darkMode ? "Light Mode" : "Dark Mode"}
+        </button>
+      </div>
+      <div className="flex flex-col md:flex-row justify-center items-center mb-8 w-full">
+        <input
+          type="text"
+          placeholder="Search by any attribute"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="px-4 py-2 text-black rounded-l-full border border-gray-400 focus:outline-none focus:ring focus:border-blue-500 flex-grow w-full md:w-96 bg-slate-100 dark:bg-gray-700 dark:text-white dark:border-gray-600 mb-4 md:mb-0"
+        />
+        <Button
+          onClick={() => setIsOpen(true)}
+          loading={sending}
+          className="rounded-r-full md:ml-2 w-full md:w-auto"
+        >
+          Add New Data
+        </Button>
+      </div>
+      <div className="overflow-x-auto w-full flex-grow">
+        <Table
+          entries={filteredEntries}
+          onEdit={handleEdit}
+          onDelete={handleDelete}
+          onSelect={handleSelect}
+        />
+      </div>
+      <div className="flex justify-center mt-8 w-full">
+        <Button onClick={handleSendEmail} loading={sending} className="w-full md:w-auto">
+          {sending ? "Sending Email..." : "Send Email"}
+        </Button>
+      </div>
+      <FormModal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        onSave={handleSave}
+      />
+      <FormUpdate
+        isOpenUpdate={isOpenUpdate}
+        onCloseUpdate={() => setIsOpenUpdate(false)}
+        onSaveUpdate={handleSaveUpdate}
+        data={updateSelect}
+      />
+    </div>
   );
 }
